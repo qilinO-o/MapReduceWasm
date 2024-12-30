605241c47624967539fe95c752457245ba4c2ed6
qilinO-o <huangdh_0314@163.com>
2024-12-30 19:26:18 +0800

    fix multi init

--- worker/src/wasmmaprt.rs
+++ worker/src/wasmmaprt.rs
@@ -9,21 +9,36 @@ bindgen!({
     async: false
 });
 
-pub fn do_wasm_map(wasm_binary: &Vec<u8>, key: &String, value: &String) -> wasmtime::Result<Vec<(String, String)>> {
-    let engine = Engine::default();
-    // Construct component
-    let component = Component::from_binary(&engine, &wasm_binary).context("Failed to load map component binary")?;
-        
-    // Construct store for storing running states of the component
-    let wasi_view = States::new();
-    let mut store = Store::new(&engine, wasi_view);
-    // Construct linker for linking interfaces.
-    // For this simple adder component, no need to link additional interfaces.
-    let mut linker = Linker::new(&engine);
-    wasmtime_wasi::add_to_linker_sync(&mut linker)?;
-    let instance = Mapper::instantiate(&mut store, &component, &linker)
-        .context("Failed to instantiate the mapper world")?;
-    instance
-        .interface0.call_map(&mut store, &key, &value)
-        .context("Failed to call map function")
+pub struct WasmMapRuntime {
+    store: Store<States>,
+    instance: Mapper,
+}
+
+impl WasmMapRuntime {
+    pub fn new(wasm_binary: &Vec<u8>) -> anyhow::Result<Self> {
+        let engine = Engine::default();
+        // Construct component
+        let component = Component::from_binary(&engine, &wasm_binary).context("Failed to load map component binary")?;
+            
+        // Construct store for storing running states of the component
+        let wasi_view = States::new();
+        let mut store = Store::new(&engine, wasi_view);
+        // Construct linker for linking interfaces.
+        // For this simple adder component, no need to link additional interfaces.
+        let mut linker = Linker::new(&engine);
+        wasmtime_wasi::add_to_linker_sync(&mut linker)?;
+        let instance = Mapper::instantiate(&mut store, &component, &linker)
+            .context("Failed to instantiate the mapper world")?;
+        Ok(Self {
+            store,
+            instance,
+        })
+    }
+
+    pub fn do_map(&mut self, key: &String, value: &String) -> wasmtime::Result<Vec<(String, String)>> {
+        self.instance
+            .interface0.call_map(&mut self.store, &key, &value)
+            .context("Failed to call map function")
+    }
+    
 }
--- worker/src/wasmreducert.rs
+++ worker/src/wasmreducert.rs
@@ -9,21 +9,36 @@ bindgen!({
     async: false
 });
 
-pub fn do_wasm_reduce(wasm_binary: &Vec<u8>, key: &String, values: &Vec<String>) -> wasmtime::Result<(String, String)> {
-    let engine = Engine::default();
-    // Construct component
-    let component = Component::from_binary(&engine, &wasm_binary).context("Failed to load reduce component binary")?;
-        
-    // Construct store for storing running states of the component
-    let wasi_view = States::new();
-    let mut store = Store::new(&engine, wasi_view);
-    // Construct linker for linking interfaces.
-    // For this simple adder component, no need to link additional interfaces.
-    let mut linker = Linker::new(&engine);
-    wasmtime_wasi::add_to_linker_sync(&mut linker)?;
-    let instance = Reducer::instantiate(&mut store, &component, &linker)
-        .context("Failed to instantiate the reducer world")?;
-    instance
-        .interface0.call_reduce(&mut store, &key, &values)
-        .context("Failed to call reduce function")
+pub struct WasmReduceRuntime {
+    store: Store<States>,
+    instance: Reducer,
+}
+
+impl WasmReduceRuntime {
+    pub fn new(wasm_binary: &Vec<u8>) -> anyhow::Result<Self> {
+        let engine = Engine::default();
+        // Construct component
+        let component = Component::from_binary(&engine, &wasm_binary).context("Failed to load reduce component binary")?;
+            
+        // Construct store for storing running states of the component
+        let wasi_view = States::new();
+        let mut store = Store::new(&engine, wasi_view);
+        // Construct linker for linking interfaces.
+        // For this simple adder component, no need to link additional interfaces.
+        let mut linker = Linker::new(&engine);
+        wasmtime_wasi::add_to_linker_sync(&mut linker)?;
+        let instance = Reducer::instantiate(&mut store, &component, &linker)
+            .context("Failed to instantiate the reducer world")?;
+        Ok(Self {
+            store,
+            instance,
+        })
+    }
+
+    pub fn do_reduce(&mut self, key: &String, values: &Vec<String>) -> wasmtime::Result<(String, String)> {
+        self.instance
+            .interface0.call_reduce(&mut self.store, &key, &values)
+            .context("Failed to call reduce function")
+    }
+    
 }
